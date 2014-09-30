a4c62678ee5de8cfb57eb94e7c12ffb9f4b3bde9
40777 <40777@PC004148.coll.lr.local>
2014-09-30 12:46:47 +0100

    revision exercise 1

---
+++ revision exercise 1.py
@@ -0,0 +1,14 @@
+#christopher pullen
+#30-09-2014
+#revision exercise 1
+
+print("hello :) ")
+
+value_1=int(input("please enter your first number"))
+value_2=int(input("please enter your second number"))
+
+if value_1==value_2:
+    print("Your numbers are the same !!! :P ")
+else:
+    print ("your numbers are not the same :(")
+    
