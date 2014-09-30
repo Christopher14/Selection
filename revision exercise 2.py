8e7c5cfc50c09fa97e7f42f88ff57be38f042538
40777 <40777@PC004148.coll.lr.local>
2014-09-30 13:00:58 +0100

    age on car thingy

---
+++ revision exercise 2.py
@@ -0,0 +1,10 @@
+#Christopher Pullen
+#30-09-2014
+#Revision exercise 2
+
+age = int(input("please enter your age:"))
+
+if age >= 17:
+    print ("you are legally able to drive a car with learner plates")
+else:
+    print ("you are not legally able to drive a car")
