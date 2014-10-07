a3843ba65bd603e80a6d20be48672d149e7c2a29
40777 <40777@PC004147.coll.lr.local>
2014-10-07 13:00:42 +0100

    :)

---
+++ revision exercise 5.py
@@ -0,0 +1,17 @@
+#Christopher pullen
+#7-10/2014
+#statement class exercise revision exercise 5
+
+print ("hello")
+
+number = int(input("Please enter a number between 21 and 29:"))
+
+if 21 <= number <=29:
+    print ("your number is within the range provided.")
+
+elif number < 21:
+    print ("your number is lower than the provided range")
+
+else:
+    print ("your number is higher than the provided range")
+   
